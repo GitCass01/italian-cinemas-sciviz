b4c4118b35fce33fa7ee02b5f38b5beea91754ff
GitCass01 <72577093+GitCass01@users.noreply.github.com>
2022-11-22 18:24:16 +0100

    better project structure

--- utility.py
+++ utility.py
@@ -27,3 +27,27 @@ film_ita = combine_csv(path, 'film_ita.csv')                                 # F
 costo_biglietti = pd.read_csv(path + 'costo_biglietti.csv', sep=',')         # FONTE: SIAE
 num_luoghi = pd.read_csv(path + 'num_luoghi.csv', sep=',')                   # FONTE: SIAE
 info_cinema = pd.read_csv(path + 'info_cinema.csv', sep=',')                 # FONTE: SIAE
+
+# ritorna la top X dei box office di ogni anno
+# X = ultima posizione da prendere dai box office
+def get_topX(pos):
+    lst = []
+    for year in range(2005, 2022):
+        for i in range(0, pos):
+                lst.append(box_offices[box_offices['Year'] == year].iloc[i])
+    top = pd.DataFrame(lst)
+    return top
+
+# ritorna il numero di film nella "top" di una determinata "nazione"
+def get_naz_top(top, nazione):
+    countbynaz = top.groupby(['Year', 'Naz.']).size().to_frame(name='count').reset_index()
+    x = countbynaz['Year'].unique()
+    naz_top = countbynaz[countbynaz['Naz.'] == nazione]
+
+    naz_years = countbynaz[countbynaz['Naz.'] == nazione]['Year'].tolist()
+    for year in range(2005, 2022):
+        if year not in naz_years:
+            naz_top = pd.concat([naz_top, pd.DataFrame.from_records([{'Year':year, 'Naz.':nazione, 'count':0}])])
+
+    naz_top = naz_top.sort_values(by=['Year'])['count']
+    return naz_top
